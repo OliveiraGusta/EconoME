07e4a348bc85c3c4dbfcc9e1d6b8e1f6070af862
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-02-05 00:21:38 -0300

    Created Function to convert amont to Brazilian Real

--- scripts.js
+++ scripts.js
@@ -70,11 +70,11 @@ const DOM = {
    innerHTMLTransaction(transaction) {
       const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
-      // const amount = 
+      const amount = Utils.formatCurrency(transaction.amount)
 
       const html = `      
             <td class="description">${transaction.description}</td>
-            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date} </td>
             <td>
                <img src="./assets/minus.svg" alt="Remover transação">
@@ -85,7 +85,22 @@ const DOM = {
    }
 }
 
+const Utils ={
+   formatCurrency(value){
+      const signal = Number(value) < 0 ? "-" : ""
 
+      value = String(value).replace(/\D/g, "")
+
+      value = Number(value) / 100
+
+      value = value.toLocaleString("pt-BR", {
+         style: "currency",
+         currency:"BRL"
+      })
+
+      return signal + value
+   }
+}
 
 transactions.forEach((transaction) => {
    DOM.addTransaction(transaction)
