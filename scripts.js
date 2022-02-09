e771b5c5e8990c109208279dd8666940ae42d2a3
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-02-09 09:03:42 -0300

    Transaction.remove

--- scripts.js
+++ scripts.js
@@ -92,14 +92,15 @@ const DOM = {
 
    addTransaction(transaction, index) {
       const tr = document.createElement('tr')
-      tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+      tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+      tr.dataset.index = index
 
       DOM.transactionsContainer.appendChild(tr)
 
 
    },
 
-   innerHTMLTransaction(transaction) {
+   innerHTMLTransaction(transaction, index) {
       const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
       const amount = Utils.formatCurrency(transaction.amount)
@@ -109,7 +110,7 @@ const DOM = {
                <td class="${CSSclass}">${amount}</td>
                <td class="date">${transaction.date} </td>
                <td>
-                  <img src="./assets/minus.svg" alt="Remover transação">
+                  <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
                </td> 
                `
 
@@ -202,20 +203,28 @@ const Form={
       }
    },
 
+   clearFields(){
+      Form.description.value = ""
+      Form.amount.value = ""
+      Form.date.value = ""
+   },
+
+
    submit(event){
       event.preventDefault()
 
       try{  
          //verificar se as informaçoes forma preenchidas
-         //Form.validateFields()
-
+         Form.validateFields()
          // formatar os dados para salvar
          const transaction = Form.formatValues()
          // salvar
-         
-         // Informaçoes apagadas
+         Transaction.add(transaction)
+         //Apagar Informaçoes 
+         Form.clearFields()
          // fechar modal
-         // atualizar
+         Modal.close()
+         
 
       } catch (error){
          alert(error.message)
@@ -227,10 +236,7 @@ const Form={
 const App = {
    init() {
 
-      Transaction.all.forEach(transaction => {
-         DOM.addTransaction(transaction)
-      })
-
+      Transaction.all.forEach(DOM.addTransaction)
       DOM.updateBalance()
 
    },
